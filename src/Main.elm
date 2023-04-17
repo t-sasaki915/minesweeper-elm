be71399a5eb0c3cdf4668246566121fb904eb37c
stouma915 <sasakitouma@yahoo.ne.jp>
2023-04-17 16:03:40 +0900

    Add Main.elm

---
+++ src/Main.elm
@@ -0,0 +1,30 @@
+module Main exposing ( main )
+
+import Browser
+import Html exposing ( Html, button, div, text )
+import Html.Events exposing ( onClick )
+
+type alias Model = Int
+type Msg = Increment | Decrement
+
+main : Program () Model Msg
+main =
+  Browser.sandbox
+    { init = 0
+    , update = update
+    , view = view
+    }
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Decrement] [ text "-" ]
+    ]
