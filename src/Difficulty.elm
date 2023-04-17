01d1ffdcb848dd873336a4731fd98ac55a3b6abf
stouma915 <sasakitouma@yahoo.ne.jp>
2023-04-17 16:39:23 +0900

    Update Difficulty.elm

--- src/Difficulty.elm
+++ src/Difficulty.elm
@@ -1,15 +1,30 @@
 module Difficulty exposing (..)
 
-type Difficulty = Difficulty String String Int Int Int
+type alias Difficulty = { name : String
+                        , displayName : String
+                        , width: Int
+                        , height: Int
+                        , mineCount: Int
+                        }
 
 easy : Difficulty
 easy = Difficulty "easy" "Easy" 9 9 10
-
 normal : Difficulty
 normal = Difficulty "normal" "Normal" 16 16 40
-
 hard : Difficulty
 hard = Difficulty "hard" "Hard" 30 16 99
-
 impossible : Difficulty
 impossible = Difficulty "impossible" "Impossible" 9 9 67
+
+defaultDifficulty : Difficulty
+defaultDifficulty = easy
+
+allDifficulties : List Difficulty
+allDifficulties = [ easy
+                  , normal
+                  , hard
+                  , impossible
+                  ]
+
+fromString : String -> Maybe Difficulty
+fromString str = find ((name _) == str) allDifficulties
