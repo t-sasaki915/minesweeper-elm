0ac3a48b9281b675d56d070a1d570c6afcedf02a
stouma915 <sasakitouma@yahoo.ne.jp>
2023-04-21 08:35:33 +0900

    Add ListUtil

---
+++ src/ListUtil.elm
@@ -0,0 +1,44 @@
+module ListUtil exposing
+    ( contains
+    , count
+    , exists
+    , listWith
+    , listWithout
+    , nonEmpty
+    , numberOf
+    )
+
+
+nonEmpty : List a -> Bool
+nonEmpty list =
+    not (List.isEmpty list)
+
+
+exists : (a -> Bool) -> List a -> Bool
+exists func list =
+    nonEmpty (List.filter func list)
+
+
+contains : a -> List a -> Bool
+contains a list =
+    exists (\x -> x == a) list
+
+
+listWith : a -> List a -> List a
+listWith a list =
+    a :: list
+
+
+listWithout : a -> List a -> List a
+listWithout a list =
+    List.filter (\x -> not (x == a)) list
+
+
+count : (a -> Bool) -> List a -> Int
+count func list =
+    List.length (List.filter func list)
+
+
+numberOf : a -> List a -> Int
+numberOf a list =
+    count (\x -> x == a) list
