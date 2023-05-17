b25c07d4ed85dd3b5b51db88683d415da769940a
stouma915 <sasakitouma@yahoo.ne.jp>
2023-05-17 12:55:29 +0900

    Refactor Images

--- src/Images.elm
+++ src/Images.elm
@@ -17,18 +17,16 @@ mineIcon =
         [ circle [ cx "15", cy "15", r "11" ] []
         , rect [ x "14", y "0", width "2", height "30" ] []
         , rect [ x "0", y "14", width "30", height "2" ] []
-        , rect [ x "20", y "-14", width "1.5", height "28", transform "rotate(45)" ] []
-        , rect [ x "7", y "0", width "28", height "1.5", transform "rotate(45)" ] []
+        , polygon [ points "5 6, 6 5, 25 24, 24 25" ] []
+        , polygon [ points "25 6, 24 5, 5 24, 6 25" ] []
         ]
 
 
 flagIcon : Html a
 flagIcon =
     svg [ width "22", height "22" ]
-        [ rect [ x "5", y "15", width "11", height "5" ] []
-        , polygon [ points "5 15, 0 22, 20 22" ] []
-        , polygon [ points "16 15, 22 22, 15 22" ] []
-        , rect [ x "10", y "1", width "1.5", height "16" ] []
+        [ polygon [ points "0 22, 5 15, 17 15, 22, 22" ] []
+        , rect [ x "10", y "1", width "1.5", height "14" ] []
         , polygon [ points "10 1, 10 10, 1 5", fill "red" ] []
         ]
 
@@ -36,10 +34,8 @@ flagIcon =
 fakeFlagIcon : Html a
 fakeFlagIcon =
     svg [ width "22", height "22" ]
-        [ rect [ x "5", y "15", width "11", height "5", fill "gray" ] []
-        , polygon [ points "5 15, 0 22, 20 22", fill "gray" ] []
-        , polygon [ points "16 15, 22 22, 15 22", fill "gray" ] []
-        , rect [ x "10", y "1", width "1.5", height "16", fill "gray" ] []
+        [ polygon [ points "0 22, 5 15, 17 15, 22, 22", fill "gray" ] []
+        , rect [ x "10", y "1", width "1.5", height "14", fill "gray" ] []
         , polygon [ points "10 1, 10 10, 1 5", fill "gray" ] []
         ]
 
@@ -47,10 +43,8 @@ fakeFlagIcon =
 wrongFlagIcon : Html a
 wrongFlagIcon =
     svg [ width "22", height "22" ]
-        [ rect [ x "5", y "15", width "11", height "5" ] []
-        , polygon [ points "5 15, 0 22, 20 22" ] []
-        , polygon [ points "16 15, 22 22, 15 22" ] []
-        , rect [ x "10", y "1", width "1.5", height "16" ] []
+        [ polygon [ points "0 22, 5 15, 17 15, 22, 22" ] []
+        , rect [ x "10", y "1", width "1.5", height "14" ] []
         , polygon [ points "10 1, 10 10, 1 5", fill "red" ] []
         , polygon [ points "2 0, 0 0, 20 22, 22 22", fill "red" ] []
         , polygon [ points "22 0, 20 0, 0 22, 2 22", fill "red" ] []
